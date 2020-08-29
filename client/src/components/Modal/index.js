11c7718dcbadb21838715d51576e8102a9545036
Greg Allebach <greg.allebach@gmail.com>
2020-08-29 14:07:40 -0400

    set up modal for scheduler

---
+++ client/src/components/Modal/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function CenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    {props.thisDate}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Button className="mr-3" href="#">Create Event</Button>
+                <Button className="mr-3" href="#">Find Events</Button>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default CenteredModal;
--- client/src/pages/scheduler.js
+++ client/src/pages/scheduler.js
@@ -1,43 +1,59 @@
-import React, { Component } from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
+import React, { Component, useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
+import CenteredModal from "../components/Modal";
 
 class Scheduler extends Component {
 
   state = {
-    savedDates: []
+    savedDates: [],
+    modalShow: false,
+    thisDate: ""
   }
 
   componentDidMount() {
     this.getDates();
   }
 
-getDates = () => {
+  getDates = () => {
     fetch("/api/calendar")
-        .then(res => res.json())
-        .then((dates) => {
-            console.log(dates);
-            this.setState({ savedDates: dates })
-        })
-        .catch(err => console.log(err));
-}
+      .then(res => res.json())
+      .then((dates) => {
+        console.log(dates);
+        this.setState({ savedDates: dates })
+      })
+      .catch(err => console.log(err));
+  }
+
+  setModalShow = bVal => {
+    this.setState({ modalShow: bVal });
+  };
+
+  handleDateClick = (arg) => {
+    this.setState({ modalShow: true, thisDate: arg.dateStr });
+    console.log(this.state.savedDates);
+  };
 
   render() {
     return (
-      <FullCalendar
-        plugins={[ dayGridPlugin, interactionPlugin ]}
-        dateClick={this.handleDateClick}
-        initialView="dayGridMonth"
-        events={this.state.savedDates}
-      />
+      <div className="container">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin]}
+          dateClick={this.handleDateClick}
+          initialView="dayGridMonth"
+          events={this.state.savedDates}
+        />
+        <CenteredModal
+          show={this.state.modalShow}
+          onHide={() => this.setModalShow(false)}
+          thisDate={this.state.thisDate}
+        />
+      </div>
     )
   }
 
-  handleDateClick = (arg) => {
-      //replace this with modal to create event?
-    alert(arg.dateStr)
-  }
+
 
 }
 
