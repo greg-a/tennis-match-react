600cc92636b34c31722ba58f6806c3bae13a1d86
Patrick Urbankowski <purbankowski@gmail.com>
2020-09-15 18:38:00 -0400

    made availability and location dropdowns mat-ui components

--- client/src/components/NewEventForm/index.js
+++ client/src/components/NewEventForm/index.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 // import TextField from '@material-ui/core/TextField';
 // import { Container, Row, Col } from "../Grid";
-import { makeStyles, TextField, Button, Grid, Box } from '@material-ui/core';
+import { makeStyles, TextField, Button, Grid, Box, MenuItem, FormHelperText, FormControl, Select, InputLabel } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -16,17 +16,24 @@ const useStyles = makeStyles((theme) => ({
         marginTop: "10px",
         marginBottom: "10px"
     },
-    // availabilityForm: {
-    //     marginTop: "10px"
-    // },
-    // availabilityHeader: {
-    //     textAlign: "center",
-    //     // fontFamily: "coolvetica"
-    // },
-    availabilityInstr: {
-        textAlign: "center",
-        fontWeight: "bold"
-    }
+    root: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        }
+    },
+    formControlAvailabilty: {
+        margin: theme.spacing(1),
+        minWidth: 180,
+    },
+    formControlLocation: {
+        margin: theme.spacing(1),
+        minWidth: 300,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+
 }));
 
 function NewEventForm(props) {
@@ -36,91 +43,121 @@ function NewEventForm(props) {
         <div>
             <Box marginTop="10px" textAlign="center">
                 <Grid container spacing={3}>
-                    
+
                     <Grid item xs={12}>
                         <h2>Availability</h2>
                         <div><p>{props.instructions}</p></div>
                     </Grid>
-                    <Grid item xs={12} sm={3}>
-                        <div className="form-group">
-                            <label for="eventTitle">Availability Type</label>
-                            <select className="form-control" name="eventTitle" id="eventTitle" onChange={props.handleInputChange} value={props.eventTitle}>
-                                <option value="Casual">Casual</option>
-                                <option value="Competitive">Competitive</option>
-                            </select>
-                        </div>
+                    <Grid item xs={12} sm={4}>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+
+                            <FormControl className={classes.formControlAvailabilty}>
+                                <InputLabel id="availability-type">Availability Type</InputLabel>
+                                <Select
+                                    id="eventTitle"
+                                    select
+                                    value={props.eventTitle}
+                                    name="eventTitle"
+                                    onChange={props.handleInputChange}
+                                >
+                                    <MenuItem key="Casual" value="Casual">
+                                        Casual
+                                    </MenuItem>
+                                    <MenuItem key="Casual" value="Competitive">
+                                        Competitive
+                                    </MenuItem>
+                                </Select>
+                                {/* <FormHelperText>Availability Type</FormHelperText> */}
+                            </FormControl>
+                        </Box>
                     </Grid>
-                    <Grid item xs={12} sm={9}>
-                        <div className="form-group">
-                            <label for="eventLocation">Court Location</label>
-                            <select className="form-control" name="eventLocation" id="eventLocation" onChange={props.handleInputChange} value={props.eventLocation}>
-                                {props.courtList.map((event, i) => (
-                                    <option value={event} key={i}>{event}</option>
+                    <Grid item xs={12} sm={8}>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+
+                            <FormControl className={classes.formControlLocation}>
+                                <InputLabel id="event-location">Event Location</InputLabel>
+                                <Select
+                                    id="eventLocation"
+                                    select
+                                    value={props.eventLocation}
+                                    name="eventLocation"
+                                    onChange={props.handleInputChange}
+                                >
+                                    {props.courtList.map((event, i) => (
+                                    <MenuItem key={i} value={event}>
+                                        {event}
+                                    </MenuItem>
                                 ))}
-                            </select>
-                        </div>
+                                </Select>
+                                {/* <FormHelperText>Event Location</FormHelperText> */}
+                            </FormControl>
+                        </Box>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <form className={classes.container} noValidate>
-                            <TextField
-                                id="newDate"
-                                name="newDate"
-                                label="Date"
-                                type="date"
-                                // defaultValue="2017-05-24"
-                                value={props.newDate}
-                                onChange={props.handleInputChange}
-                                className={classes.textField}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                            />
-                        </form>
+                    <Grid item xs={12} sm={4}>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <form className={classes.container} noValidate>
+                                <TextField
+                                    id="newDate"
+                                    name="newDate"
+                                    label="Date"
+                                    type="date"
+                                    // defaultValue="2017-05-24"
+                                    value={props.newDate}
+                                    onChange={props.handleInputChange}
+                                    className={classes.textField}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </form>
+                        </Box>
                     </Grid>
-                    <Grid item xs={12} sm={3}>
-                        <form className={classes.container} noValidate>
-                            <TextField
-                                id="startTime"
-                                name="startTime"
-                                label="Start Time"
-                                type="time"
-                                // defaultValue="17:00"
-                                value={props.startTime}
-                                onChange={props.handleInputChange}
-                                className={classes.textField}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                inputProps={{
-                                    step: 300, // 5 min
-                                }}
-                            />
-                        </form>
+                    <Grid item xs={12} sm={4}>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <form className={classes.container} noValidate>
+                                <TextField
+                                    id="startTime"
+                                    name="startTime"
+                                    label="Start Time"
+                                    type="time"
+                                    // defaultValue="17:00"
+                                    value={props.startTime}
+                                    onChange={props.handleInputChange}
+                                    className={classes.textField}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    inputProps={{
+                                        step: 300, // 5 min
+                                    }}
+                                />
+                            </form>
+                        </Box>
                     </Grid>
-                    <Grid item xs={12} sm={3}>
-                        <form className={classes.container} noValidate>
-                            <TextField
-                                id="endTime"
-                                name="endTime"
-                                label="End Time"
-                                type="time"
-                                // defaultValue="18:30"
-                                value={props.endTime}
-                                onChange={props.handleInputChange}
-                                className={classes.textField}
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                inputProps={{
-                                    step: 300, // 5 min
-                                }}
-                            />
-                        </form>
+                    <Grid item xs={12} sm={4}>
+                        <Box display="flex" justifyContent="center" alignItems="center">
+                            <form className={classes.container} noValidate>
+                                <TextField
+                                    id="endTime"
+                                    name="endTime"
+                                    label="End Time"
+                                    type="time"
+                                    // defaultValue="18:30"
+                                    value={props.endTime}
+                                    onChange={props.handleInputChange}
+                                    className={classes.textField}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    inputProps={{
+                                        step: 300, // 5 min
+                                    }}
+                                />
+                            </form>
+                        </Box>
                     </Grid>
                     <Grid item xs={12}>
-                    <button type="button" className="btn"
-            onClick={props.handleFormSubmit}
-            id="login-button">Submit</button>
+                        <Button variant="contained" id="login-button" onClick={props.handleFormSubmit}>Submit</Button>
                     </Grid>
                 </Grid>
 
@@ -134,7 +171,7 @@ function NewEventForm(props) {
 
 
 
-        
+
 
     )
 }
